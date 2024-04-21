1ec0a9e85078e1040f699e0fb087bcaaf5694550
naipret <ngvphu235e@gmail.com>
2024-04-21 13:45:27 +0000

    cses 1646

---
+++ CSES/task-1646.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+// #include <numeric>  // std::partial_sum
+
+// using namespace std;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int col{}, que{};
+    std::cin >> col >> que;
+    int arr[col + 1];
+    arr[0] = 0;
+    for (int c{1}; c <= col; ++c) {
+        std::cin >> arr[c];
+    }
+    long long pre[col + 1];
+    pre[0] = 0LL;
+    for (int c{1}; c <= col; ++c) {
+        pre[c] = pre[c - 1] + arr[c];
+    }
+    // std::partial_sum(arr, arr + (col + 1), pre);
+
+    int l{}, r{};
+    while (que--) {
+        std::cin >> l >> r;
+        long long ans{pre[r] - pre[l - 1]};
+        std::cout << ans << '\n';
+    }
+
+    return 0;
+}
